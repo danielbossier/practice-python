be4b96b1e90b4eec54eaacebcd6bf14dbc155175
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-09-01 22:02:57 -0700

    More Hashes

--- trello-python.py
+++ trello-python.py
@@ -222,3 +222,24 @@
 
 # print(most_frequent("peter piper picked a peck of pickled peppers"))
 
+# Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+# Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+# Output: {"Dewey" => 6, "Truman" => 5}
+
+# Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+def votes(string):
+    i = 0
+    output = {}
+    while i < len(string):
+        if string[i] in output:
+            output[string[i]] += 1
+        else:
+            output[string[i]] = 1
+        i += 1
+    print(output)
+
+
+print(votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]))
