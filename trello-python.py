62a9a564cb7909825f33d1386f66b3c95a228a11
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-09-05 22:09:17 -0700

    Hamming

--- trello-python.py
+++ trello-python.py
@@ -389,3 +389,24 @@
 #         i2 -= 1
 #     return True
 # print(is_palindrome("oaao"))
+
+# Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+
+# Input: "ABCDEFG", "ABCXEOG"
+# Output: 2
+
+# Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different (the D and F in the first string), we return 2.
+
+# Input: "ABCDEFG", "ABCDEFG",
+# Output: 0
+
+# def hamming(string1, string2):
+#     output = 0
+#     i = 0
+#     while i < len(string1):
+#         if string1[i] != string2[i]:
+#             output += 1
+#         i += 1
+#     return output
+
+# print(hamming("ABCDEFG", "ABCXEOG"))
