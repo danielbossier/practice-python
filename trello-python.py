18d025a0f52fae697086c75aa15624942b8067a8
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-09-19 22:13:16 -0700

    Testing Author Memory

--- trello-python.py
+++ trello-python.py
@@ -766,3 +766,58 @@
 #     return False
 
 # print(sum_ten([2, 5, 3, 1, 0, 11]))
+
+# Given a list of books provided in this format:
+
+# [
+# {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+# {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+# {title: "1984", author: "George Orwell", year: 1949 },
+# {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+# {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+# {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+# {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
+# ]
+
+# return the data in this new author-centric format:
+
+# { "J. R. R. Tolkien" => [
+# {title: "The Lord of the Rings", year: 1954 },
+# {title: "The Hobbit", year: 1937 },
+# {title: "The Two Towers", year: 1954 }
+# ],
+# "Harper Lee" => [
+# {title: "To Kill a Mockingbird", year: 1960 },
+# {title: "Go Set a Watchman", year: 2015 }
+# ],
+# "George Orwell" => [
+# {title: "1984", year: 1949 }
+# ],
+# "F. Scott Fitzgerald" => [
+# {title: "The Great Gatsby", year: 1925 }
+# ]
+# }
+
+# def authors_hash(books_hash):
+#     authors_dict = {}
+
+#     for book in books_hash:
+#         author = book["author"]
+#         book_data = { "title" : book["title"], "year" : book["title"] }
+
+#         if author in authors_dict:
+#             authors_dict[author].append(book_data)
+#         else:
+#             authors_dict[author] = [book_data]
+    
+#     return authors_dict        
+
+# books = [{"title": "The Lord of the Rings", "author": "J. R. R. Tolkien", "year": 1954 },
+# {"title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960 },
+# {"title": "1984", "author": "George Orwell", "year": 1949 },
+# {"title": "Go Set a Watchman", "author": "Harper Lee", "year": 2015 },
+# {"title": "The Hobbit", "author": "J. R. R. Tolkien", "year": 1937 },
+# {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925 },
+# {"title": "The Two Towers", "author": "J. R. R. Tolkien", "year": 1954 }]
+
+# print(authors_hash(books))
