0805306f378c55825d68a10d463c89ed54aa9e09
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-09-20 18:26:31 -0700

    Nested Loops Range

--- trello-python.py
+++ trello-python.py
@@ -821,3 +821,36 @@
 # {"title": "The Two Towers", "author": "J. R. R. Tolkien", "year": 1954 }]
 
 # print(authors_hash(books))
+
+# Given a list of books provided in this format:
+
+# [
+# {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+# {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+# {title: "1984", author: "George Orwell", year: 1949 },
+# {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+# {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+# {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+# {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
+# ]
+
+# Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+
+# Specifically use nested loops to solve this exercise even though there are other approaches as well.
+
+# Input: [2, 5, 3, 1, 0, 7, 11]
+# Output: [3, 7]
+
+# Input: [1, 2, 3, 4, 5]
+# Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+def sum_ten(arr):
+    output = 0
+    for i1 in range(len(arr)):
+        for i2 in range(i1 + 1, len(arr)):
+            if i1 + i2 == 10:
+                output = i1 + i2
+                return output
+    return False
+
+print(sum_ten([2, 5, 1, 0, 7, 11]))
