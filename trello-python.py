4e34d670cb5982ee088162c08cc93757d95774f5
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-10-02 21:33:17 -0700

    Revisit

--- trello-python.py
+++ trello-python.py
@@ -985,3 +985,18 @@
 #             return False
 #     return True
 # print(is_palindrome("racecar"))
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def multiple(input):
+    i = 0
+    array = []
+    while i < input:
+        if i % 3 == 0 or i % 5 == 0:
+            array.append(i)
+        i += 1
+    return sum(array)
+
+print(multiple(1000))
