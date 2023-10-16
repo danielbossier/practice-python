7f22cc03bfa1753d61366504af78f78be923d279
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-10-16 10:53:44 -0700

    Update trello-python.py

--- trello-python.py
+++ trello-python.py
@@ -1051,4 +1051,9 @@
 #     return False
 
 # print(equal_ten([1, 2, 3, 4, 5]))
-        
+
+# Write a function that prints out every number from 1 to N, with the following exceptions:
+
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
